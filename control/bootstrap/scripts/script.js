ecd0511dbbac9c31f647ca00c7dbc6e50038d7d0
Lepkovich <6332133@gmail.com>
2023-03-04 14:41:47 +0300

    validation fixed

--- control/bootstrap/scripts/script.js
+++ control/bootstrap/scripts/script.js
@@ -7,42 +7,30 @@ $('.popup-link').magnificPopup({
 });
 
 $(document).ready(function() {
-    $('#btnSend').onclick(function(e) {
+
+    $('#inputZip').keydown(function (e) { // проверяем поле Индекс на вводимые символы
+        let number = parseInt(e.key); // мы приводим вводимые символы к цифрам. все, кроме цифр дадут NaN
+        if (isNaN(number)) { // любая нецифра (NaN) будет true
+            return false; // и мы выйдем из функции (не цифры не введутся)
+        }
+
+    });
+
+    $('#btnSend').bind('click', function (e) {
         e.preventDefault();
-        let first_name = $('#inputName').val();
-        let last_name = $('#inputSurname').val();
-        let phone = $('#inputPhone').val();
-        let country = $('#inputCountry').val();
-        let zip = $('#inputZip').val();
-        let address = $('#inputAddress').val();
-
-
-        if (first_name.length< 1) {
-            alert('Поле Имя не заполнено!');
-        }
-        if (last_name.length< 1) {
-            alert('Поле Фамилия не заполнено!');
-        }
-        if (phone.length< 1) {
-            alert('Поле Телефон не заполнено!');
-        }
-        if (country.length< 1) {
-            alert('Поле Страна не заполнено!');
-        }
-        if (zip.length< 1) {
-            alert('Поле Индекс не заполнено!');
-        }
-        zip.onkeydown = (e) => {
-            let number = parseInt(e.key); // мы приводим вводимые символы к цифрам. все, кроме цифр дадут NaN
-            if (isNaN(number)) { // не NaN будет true
-                return false; // и мы выйдем из функции (не цифры не введутся)
+        for (let a = 1; a < $('input').length; a++) { // первый input не трогаем (он в шапке сайта
+            if ($('input').eq(a).val() < 1) {  // если в поле нет ни одного символа, выведем alert
+                alert('Заполните все поля');
+                return false;
+            }
+            if ($('#inputZip').val().length !==6) {
+                alert('В поле Индекс должно быть 6 цифр');
+                return false;
             }
         }
-        if (zip.length !== 6) {
-            alert('В поле Индекс должно быть 6 цифр!');
-        }
-        if (address.length< 1) {
-            alert('Поле Адрес не заполнено!');
-        }
+        $('#makeOrder').addClass("hideElement");
+        $('#thankYou').removeClass("hideElement");
+
     });
 });
+
