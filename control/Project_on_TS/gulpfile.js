e232850e2d867af0c0fc14246cb8c09c6ac0f640
Lepkovich <6332133@gmail.com>
2023-03-14 13:59:58 +0300

    add gulp, less, fonts. start styles.less

--- control/Project_on_TS/gulpfile.js
+++ control/Project_on_TS/gulpfile.js
@@ -1,17 +1,17 @@
 'use strict';
 const {src, dest, watch, series} = require('gulp');
 const less = require('gulp-less');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
+// const cssmin = require('gulp-cssmin');
+// const rename = require('gulp-rename');
 // const watchLess = require('gulp-watch-less');
 
 
 exports.less = function() {
-    return src('./src/css/css.less') // берем наш объединенный less файл из папки
+    return src('./src/css/styles.less') // берем наш объединенный less файл из папки
         // .pipe(watchLess('./src/css/*.less')) // отслеживаем все изменения в файлах less
         .pipe(less()) // преобразуем его в css ...
-        .pipe(cssmin()) // минифицируем наш css
-        .pipe(rename({suffix: '.min'})) // переименовываем в min.css
+        // .pipe(cssmin()) // минифицируем наш css
+        // .pipe(rename({suffix: '.min'})) // переименовываем в min.css
         .pipe(dest('./dist')); // размещаем в папку dist
 }
 
