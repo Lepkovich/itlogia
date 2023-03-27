17f5e7705935081fa6712edf5a2dfe53aeaa1ed0
Lepkovich <6332133@gmail.com>
2023-03-27 19:56:45 +0300

    make click watching on techno area with house

--- control/Project_on_TS/scripts/script.js
+++ control/Project_on_TS/scripts/script.js
@@ -21,14 +21,38 @@ $(document).ready(function() {
         $('#trigger').css('display', 'none'); // и уберем саму надпись
     })
     //----------------- отслеживаем клики на кружочки по дому
-    $('.tech-img').on('click', function () {
-      for (let i=1; i<6; i++) {
-          //$('.tech-(i)-text')
+
+    $('.tech-img').on('click', function () {  //отследим клик на любую область дома
+        for (let i=1; i<6; i++) {
+          $('.' + "tech-" + i + "-text").css('display', 'none'); // уберем все текстовые описания
+          $('#' + "round-" + i).removeClass('active-round'); // уберем оформление нажатого кружка
       }
     })
-    $('#round-1').on('click', function (){
-        $('#tech-1').addClass('active-round');  // добавили класс (изменить цвет кружка и убрать анимацию)
-        $('.tech-1-text').css('display', 'block'); // отобразить описание
 
-    })
+    $('#round-1').click(function( event ) { // отслеживаем клик по кружочку
+        event.stopPropagation();
+        $('#round-1').addClass('active-round');  // добавили класс (изменить цвет кружка и убрать анимацию)
+        $('.tech-1-text').css('display', 'block'); // отобразить тестовый блок с описанием
+    });
+    $('#round-2').click(function( event ) {
+        event.stopPropagation();
+        $('#tech-2').addClass('active-round');
+        $('.tech-2-text').css('display', 'block');
+    });
+    $('#round-3').click(function( event ) {
+        event.stopPropagation();
+        $('#tech-3').addClass('active-round');
+        $('.tech-3-text').css('display', 'block');
+    });
+    $('#round-4').click(function( event ) {
+        event.stopPropagation();
+        $('#tech-4').addClass('active-round');
+        $('.tech-4-text').css('display', 'block');
+    });
+    $('#round-5').click(function( event ) {
+        event.stopPropagation();
+        $('#tech-5').addClass('active-round');
+        $('.tech-5-text').css('display', 'block');
+    });
+    // и попробовать все их в цикл засунуть с i
 });
